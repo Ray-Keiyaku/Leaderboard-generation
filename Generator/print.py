083a23bf2f8cb34864a5f384d09c3181d0c5ac79
Ray <zrztt@outlook.com>
2020-07-19 02:19:22 +0800

    Fix(print): too many prints

--- Generator/print.py
+++ Generator/print.py
@@ -29,28 +29,28 @@ def print_table(table_now, table_content):
     for num, summary in enumerate(table_content):
         y = start_y + (num + 2) * line_spacing
         draw.text((start_x, y), u'%s' % str(num + 1), color, font)
-        if num > 14:
+        if num >= 14:
             break
 
     draw.text((start_x + 100, start_y + line_spacing), "名称", color, font)
     for num, summary in enumerate(table_content):
         y = start_y + (num + 2) * line_spacing
         draw.text((start_x + 100, y), u'%s' % summary[0], color, font)
-        if num > 14:
+        if num >= 14:
             break
 
     draw.text((start_x + 450, start_y + line_spacing), "总分", color, font)
     for num, summary in enumerate(table_content):
         y = start_y + (num + 2) * line_spacing
         draw.text((start_x + 450, y), u'%s' % summary[8], color, font)
-        if num > 14:
+        if num >= 14:
             break
 
     draw.text((start_x + 550, start_y + line_spacing), "推荐", color, font)
     for num, summary in enumerate(table_content):
         y = start_y + (num + 2) * line_spacing
         draw.text((start_x + 550, y), u'%s' % summary[9], color, font)
-        if num > 14:
+        if num >= 14:
             break
 
     # 保存图片
