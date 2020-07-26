bb723490a60f843a563fde33aa8e0e1b897db169
Ray <zrztt@outlook.com>
2020-07-27 01:22:10 +0800

    fix(data): Recommended score weight

--- globalData/data.py
+++ globalData/data.py
@@ -16,7 +16,7 @@ PRINT_FONT_PATH = os.path.join(TOP_PATH, res["print_font"])
 # Weight
 INIT_LIST = ["动漫", "电影", "电视剧"]
 WEIGHT_TOTAL = {'画风': 1 / 90, '画质': 2 / 90, '音乐': 1 / 90, '语音': 1 / 90, '设定': 1 / 90, '剧情': 1 / 90, '角色': 2 / 90}
-WEIGHT_RECOMMEND = {'画风': 0, '画质': 2 / 60, '音乐': 1 / 60, '语音': 1 / 60, '设定': 1 / 60, '剧情': 1 / 60, '角色': 0}
+WEIGHT_RECOMMEND = {'画风': 0, '画质': 2 / 50, '音乐': 1 / 50, '语音': 1 / 50, '设定': 1 / 50, '剧情': 0, '角色': 0}
 
 # Print Parameter
 PRINT_FONT_SIZE = 24
